16496c5110841a97ce3673c5c9acc3b8f99cc9e3
zczczc236-crypto <zczczc236@gmail.com>
2026-02-11 08:40:59 +0900

    Update export.js

--- scripts/export.js
+++ scripts/export.js
@@ -1,12 +1,17 @@
-import { storage } from "../firebase/firebase.js";
-import {
-  ref,
-  uploadBytes,
-  getDownloadURL
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
+export function exportTXT(title, content) {
+  const blob = new Blob([title + "\n\n" + content], { type: "text/plain" });
+  download(blob, `${title}.txt`);
+}
 
-export async function uploadImage(file) {
-  const imageRef = ref(storage, "images/" + Date.now() + file.name);
-  await uploadBytes(imageRef, file);
-  return await getDownloadURL(imageRef);
+export function exportEPUB(title, content) {
+  const html = `<h1>${title}</h1><p>${content.replace(/\n/g, "<br>")}</p>`;
+  const blob = new Blob([html], { type: "application/epub+zip" });
+  download(blob, `${title}.epub`);
+}
+
+function download(blob, name) {
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = name;
+  a.click();
 }
