5c9c236b6861fdd911948568483d5b09e287c2dd
zczczc236-crypto <zczczc236@gmail.com>
2026-02-11 08:29:26 +0900

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,23 +1,7 @@
 import { auth } from './firebase.js';
-import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-export function login(email, pw) {
-  return signInWithEmailAndPassword(auth, email, pw);
-}
 
-export function signup(email, pw) {
-  return createUserWithEmailAndPassword(auth, email, pw);
-}
-
-export function logout() {
-  return signOut(auth);
-}
-
-export function watchAuth(cb) {
-  onAuthStateChanged(auth, cb);
-}
+export const loginUser=(e,p)=>signInWithEmailAndPassword(auth,e,p).then(()=>location.href='index.html');
+export const signupUser=(e,p)=>createUserWithEmailAndPassword(auth,e,p).then(()=>location.href='index.html');
+export const logoutUser=()=>signOut(auth).then(()=>location.href='login.html');
