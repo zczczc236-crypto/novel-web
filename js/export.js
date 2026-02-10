a1fc610eef8236a5085d4606c58525cbe9f71b1e
zczczc236-crypto <zczczc236@gmail.com>
2026-02-11 08:58:39 +0900

    Update export.js

--- js/export.js
+++ js/export.js
@@ -1,5 +1,9 @@
-TXT: Blob
-
-PDF: jsPDF
-
-EPUB: epub.js
+function exportText(){
+  const series = JSON.parse(localStorage.getItem("novel_series")||"[]");
+  const content = series.map(s=>s.text).join("\n\n");
+  const blob = new Blob([content],{type:"text/plain"});
+  const a = document.createElement("a");
+  a.href=URL.createObjectURL(blob);
+  a.download="novel.txt";
+  a.click();
+}
