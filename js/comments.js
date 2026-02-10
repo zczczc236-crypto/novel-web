e6c6a5eed2e01a378b6c8d6be6c190562f036425
zczczc236-crypto <zczczc236@gmail.com>
2026-02-11 08:25:12 +0900

    Update comments.js

--- js/comments.js
+++ js/comments.js
@@ -1,14 +1,16 @@
-js
-const firebaseConfig = {
-apiKey: "YOUR_KEY",
-authDomain: "YOUR_DOMAIN",
-databaseURL: "YOUR_DB",
-projectId: "YOUR_ID"
-};
-firebase.initializeApp(firebaseConfig);
-const commentsRef = firebase.database().ref('comments');
+import { db } from './firebase.js';
+import { ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
+export function listenComments(storyId, cb) {
+  onValue(ref(db, 'comments/' + storyId), s => {
+    cb(s.val() || {});
+  });
+}
 
-function postComment(story, ep, name, text){
-commentsRef.child(story+'-'+ep).push({name,text,time:Date.now()});
+export function addComment(storyId, name, text) {
+  push(ref(db, 'comments/' + storyId), {
+    name,
+    text,
+    time: Date.now()
+  });
 }
