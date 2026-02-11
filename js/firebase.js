eefd7533b4073177a29ba2aa6af44aeb0ad4ca6c
zczczc236-crypto <zczczc236@gmail.com>
2026-02-11 09:02:12 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,21 @@
+// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 
 const firebaseConfig = {
 apiKey: "YOUR_KEY",
-authDomain: "YOUR_ID.firebaseapp.com",
-databaseURL: "https://YOUR_ID.firebaseio.com",
-projectId: "YOUR_ID",
-storageBucket: "YOUR_ID.appspot.com",
-messagingSenderId: "XXXX",
-appId: "XXXX"
+authDomain: "YOUR_DOMAIN",
+projectId: "YOUR_PROJECT",
+storageBucket: "YOUR_BUCKET",
+messagingSenderId: "ID",
+appId: "APPID"
 };
 
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getDatabase(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
